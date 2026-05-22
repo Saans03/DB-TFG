24d83b1801611396f856d40db4df47f321163218
Saans03 <saizviecodavid@gmail.com>
2026-05-22 17:31:13 +0200

    Update Score.js

--- API/models/Score.js
+++ API/models/Score.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 const scoreSchema = new mongoose.Schema({
     playerName: { type: String, required: true },
     score: { type: Number, required: true },
-    // La nueva variable para guardar las monedas del jugador
-    monedas: { type: Number, default: 0 } ,
-    nivelMaximo: { type: String, default: "Ninguno" } // <-- NUEVO: Guarda el último nivel superado
+    monedas: { type: Number, default: 0 },
+    nivelMaximo: { type: String, default: "Ninguno" } // 👈 Asegúrate de que esta línea exista
 });
 
 module.exports = mongoose.model('Score', scoreSchema);
